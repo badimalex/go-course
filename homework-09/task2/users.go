46826e67c657d68aab88e59c59810cdbf2d91f9f
badichan dmitry <badimalex@gmail.com>
2023-08-19 11:12:02 +0300

    Task 2

--- homework-09/task2/users.go
+++ homework-09/task2/users.go
@@ -1,1 +1,31 @@
 package homework09
+
+type Employee struct {
+	Age int
+}
+
+type Customer struct {
+	Age int
+}
+
+func MaxAge(people ...any) any {
+	max := 0
+	var oldest any
+
+	for _, user := range people {
+		switch u := user.(type) {
+		case Employee:
+			if u.Age > max {
+				max = u.Age
+				oldest = u
+			}
+		case Customer:
+			if u.Age > max {
+				max = u.Age
+				oldest = u
+			}
+		}
+	}
+
+	return oldest
+}
